8d70167039b7d361f2f090fe8d55cb673c3b36cf
Git4me2show <jdkdn3732@gmail.com>
2022-12-27 12:58:19 -0500

    task 1

--- client.py
+++ client.py
@@ -35,15 +35,20 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+
+    "Using this formula we will be able to compute the right stock price"
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
 
+    "Changes made to return the value to the ratio of stock price_a to stock price_b"
+    if(price_b == 0):
+        return
+    return price_a/price_b
 
 # Main
 if __name__ == "__main__":
@@ -56,4 +61,4 @@ if __name__ == "__main__":
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % (getRatio(price['ABC'], price['DEF'])))
